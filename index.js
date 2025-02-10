6a0d9953e9c0901b3a7a8ac0bfeb5e7933f25e0d
AllenNg012 <nch0123ca@gmail.com>
2025-02-10 18:07:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ const corsOptions = {
 var cors = require('cors');
 app.use(cors())
 // Use CORS middleware with the options
+app.get('/cors', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send({ "msg": "This has CORS enabled 🎈" })
+  })
+  app.use(cors(corsOptions));
 
 // Other middlewares
 app.use(express.json());
