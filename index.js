e6662a823519b825b7a1051e8b72ff0b725ecc2a
AllenNg012 <nch0123ca@gmail.com>
2025-02-10 16:08:14 -0500

    verceltesting

---
+++ index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+require("./db/conn");
+const router = require("./routes/router");
+const cors = require("cors");
+
+
+const port = 8005;
+
+
+app.use(express.json());
+app.use(cors())
+app.use(router);
+
+app.use("/uploads",express.static("./uploads"));
+app.use("/", (req, res) => {
+    res.send("Server is running.");
+  });
+
+app.listen(port,()=>{
+    console.log(`server start at port no ${port}`)
+})
